c17b8024784003552da8f96ed22bdb75ae7f531b
Mudadaa <layeb.hassan@gmail.com>
2023-09-16 08:22:25 +0200

    server,css&index initialization

---
+++ index.js
@@ -0,0 +1,16 @@
+//on importe les dépendances
+const express=require('express');
+//on crée l'app express
+const app=express();
+
+//on renvoit la route vers le fichier index.html 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+}   );
+//on sert toutes les ressources présents dans le dossier public
+app.use(express.static(__dirname + '/public'));
+
+//on lance le serveur sur le port 5000
+app.listen(5000,()=>{
+    console.log('Server is running at port 3000');
+}   );
