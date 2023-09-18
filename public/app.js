0af4428542999cfbcf45116790ddb6dfd4113e2e
Mudadaa <layeb.hassan@gmail.com>
2023-09-18 07:38:39 +0200

    filter for lakes with region success

--- public/app.js
+++ public/app.js
@@ -1,95 +1,114 @@
 const app = {
-    //init est une fonction asynchrone car elle reprend les fonctions asynchrones
-    //et leur donne un ordre d'exécution avec les données 
-    init: async function () {
-        //les données qui vont être récupérés de l'API sont mis dans la const regionsData
-        // le this fait référence à la fonction fetchRegions
-      const regionsData = await this.fetchRegions();
-      //on récupère les noms des régions
-      const regionNames = regionsData.map(region => region.nom);
-      // on donne en paramètre les noms des régions à la fonction createRegionOptions
-      this.createRegionOptions(regionNames);
-      console.log(regionNames);
-      // on fait pareil mais pour les lacs
-        const lacsData = await this.takelakes();
-        const lacNames = lacsData.map(lac => lac.nom);
-        const lacRegion = lacsData.map(lac => lac.region);
-        console.log(lacRegion);
+  regionNames: [],
+  lacsData: [], // Store the lakes data
+
+  init: function () {
+    this.fetchRegions()
+      .then((regionsData) => {
+        this.regionNames = regionsData.map((region) => region.nom);
+        this.createRegionOptions(this.regionNames);
+        console.log(this.regionNames);
+
+        return this.takelakes();
+      })
+      .then((lacsData) => {
+        this.lacsData = lacsData; // Store the lakes data
+        const lacNames = lacsData.map((lac) => lac.nom);
         this.createLacOptions(lacNames);
-    },
-  
-    fetchRegions: async function () {
-      try {
-        //on récupère les données des régions de l'API
-        const response = await fetch('https://geo.api.gouv.fr/regions', {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-        });
-  
-        if (response.ok) {
-          const data = await response.json();
-          return data;
-        } else {
-          throw new Error(`Erreur HTTP: ${response.status} - ${response.statusText}`);
-        }
-      } catch (error) {
-        throw new Error(`Une erreur s'est produite lors de la récupération des données de l'API : ${error.message}`);
+        console.log(this.lacsData);
+        this.addRegionSelectEventListener(); // Add event listener after lakes data is loaded
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
+  fetchRegions: async function () {
+    try {
+      const response = await fetch('https://geo.api.gouv.fr/regions', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error(`Erreur HTTP: ${response.status} - ${response.statusText}`);
       }
-    },
+    } catch (error) {
+      throw new Error(`Une erreur s'est produite lors de la récupération des données de l'API : ${error.message}`);
+    }
+  },
 
-    takelakes: async function(){
-        //on récupère les données des lacs du JSON
-        //lien internet https://www.campingsluxe.fr/blog/top-20-plus-beaux-lacs-de-france
-           const response= await fetch('./lacs.json', {
-            method: 'GET',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            },
-            });
-           if (response.ok){
-                const lacsData = await response.json();
-                console.log(lacsData);
-                return lacsData;
-                
-        } else  {
-            throw new Error(`Erreur HTTP: ${response.status} - ${response.statusText}`);
-            
-        }
-    },
-  
-    createRegionOptions: function (regionNames) {
-      const regionsSelect = document.getElementById('regions');
-  
-      for (let i = 0; i < regionNames.length; i++) {
-        const option = document.createElement('option');
-        option.textContent = regionNames[i];
-        regionsSelect.appendChild(option);
-      }},
+  takelakes: async function () {
+    try {
+      const response = await fetch('./lacs.json', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
 
-      createLacOptions: function (lacNames) {
-        const lacsSelect = document.getElementById('lacs');
-    
-        for (let i = 0; i < lacNames.length; i++) {
-          const option = document.createElement('option');
-          const lacOptions=option.textContent ;
-          lacOptions= lacNames[i].filter(this.regionNames=> this.regionNames=== this.lacRegion);
-         lacsSelect.appendChild(lacOptions);
-        }
-    },
+      if (response.ok) {
+        const lacsData = await response.json();
+        console.log(lacsData);
+        return lacsData;
+      } else {
+        throw new Error(`Erreur HTTP: ${response.status} - ${response.statusText}`);
+      }
+    } catch (error) {
+      throw new Error(`Une erreur s'est produite lors de la récupération des données des lacs : ${error.message}`);
+    }
+  },
+
+  createRegionOptions: function (regionNames) {
+    const regionsSelect = document.getElementById('regions');
+
+    for (let i = 0; i < regionNames.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = regionNames[i];
+      regionsSelect.appendChild(option);
+    }
+  },
+  createLacOptions: function (lacNames) {
+    const lacsSelect = document.getElementById('lacs');
+
+    for (let i = 0; i < lacNames.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = lacNames[i];
+      lacsSelect.appendChild(option);
+    }
+  },
+
+  addRegionSelectEventListener: function () {
+    const regionsSelect = document.getElementById('regions');
+    const lacsSelect = document.getElementById('lacs');
+
+    regionsSelect.addEventListener('change', () => {
+      const selectedRegion = regionsSelect.value;
+      const filteredLacs = this.lacsData.filter((lac) => lac.region === selectedRegion);
+      const lacNames = filteredLacs.map((lac) => lac.nom);
+
+      // Clear the current options in the lakes select element
+      while (lacsSelect.options.length > 0) {
+        lacsSelect.remove(0);
+      }
+
+      // Add the filtered lake options to the lakes select element
+      this.createLacOptions(lacNames);
+    });
+  },
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  app.init();
+ });
 
 
 
 
-
-  };
-
-
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    app.init();
-  });
